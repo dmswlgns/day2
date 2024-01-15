e900c62ad6b4f8db0c2bb7d455f647e8679b3f3e
jihun <altmxjdms@naver.com>
2024-01-15 14:29:50 +0900

    day2_5

--- main.py
+++ main.py
@@ -8,11 +8,38 @@
 #
 # print(f'몫은 {quotient} 나머지는 {remainder}입니다')
 
-first_number=int(input("First number:"))
+# first_number=int(input("First number:"))
+#
+# second_number=int(input("Second number:"))
+#
+# print(f'몫은 {divmod(first_number,second_number)[0]} 나머지는 {divmod(first_number,second_number)[1]}입니다')
+#
 
-second_number=int(input("Second number:"))
+# dec=65
+# octal=0o101
+# hexa=0x41
+# binary=0b01000001
+# print(dec,octal,hexa,binary)
+#
+# print(chr(binary))
+# print(chr(dec))
+# print(chr(hexa))
+# print(chr(octal))
+#
+# print(binary)
+#
+# print(bin(binary))
 
-print(f'몫은 {divmod(first_number,second_number)[0]} 나머지는 {divmod(first_number,second_number)[1]}입니다')
+fahrenheit=float(input("화씨온도:"))
+
+print(f'섭씨온도는:{((fahrenheit-32.0)*5/9):.2f}')  #소수점 정하는법
+
+
+
+
+
+
+
 
 
 
