4f8a0b2a116ec25808ae8f2ef4a0bee27886ca4d
jihun <altmxjdms@naver.com>
2024-01-15 16:06:12 +0900

    day2_homework

--- main.py
+++ main.py
@@ -1,40 +1,13 @@
-# first_number=int(input("First number:"))
-#
-# second_number=int(input("Second number:"))
-#
-# quotient=first_number//second_number
-#
-# remainder=first_number%second_number
-#
-# print(f'몫은 {quotient} 나머지는 {remainder}입니다')
-
-# first_number=int(input("First number:"))
-#
-# second_number=int(input("Second number:"))
-#
-# print(f'몫은 {divmod(first_number,second_number)[0]} 나머지는 {divmod(first_number,second_number)[1]}입니다')
-#
-
-# dec=65
-# octal=0o101
-# hexa=0x41
-# binary=0b01000001
-# print(dec,octal,hexa,binary)
-#
-# print(chr(binary))
-# print(chr(dec))
-# print(chr(hexa))
-# print(chr(octal))
-#
-# print(binary)
-#
-# print(bin(binary))
-
-fahrenheit=float(input("화씨온도:"))
-
-print(f'섭씨온도는:{((fahrenheit-32.0)*5/9):.2f}')  #소수점 정하는법
-
-
+while True:
+    menu = input("1)화씨 -> 섭씨,2)섭씨 -> 화씨: ")
+    if menu == '1':
+        fahrenheit = float(input("화씨온도:"))
+        print(f'섭씨온도는:{((fahrenheit-32.0)*5/9):.2f}')
+    elif menu=='2':
+        celsius = float(input("섭씨온도:"))
+        print(f'화씨온도는:{((celsius * 9/5) +32):.2f}')
+    elif menu=='3':
+        break
 
 
 
